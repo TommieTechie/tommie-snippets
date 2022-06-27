9845710a19fb13d13e8df805ad55cf0cd235e293
TommiEng <TommiEng@proton.me>
2022-06-26 23:32:02 -0400

    Added JS template literals snippet 02-05-2022

---
+++ snippets/js-template-literals-02-05-2022.js
@@ -0,0 +1,15 @@
+const introduction = (name, job) => {
+  const sentence = `Hello, my name is ${firstName} and I'm a ${job}`;
+  return sentence;
+};
+
+console.log(introduction("Tommie", "Software Engineer🧑‍💻"));
+// This outputs: "Hello, my name is Tommie and I'm a Software Engineer🧑‍💻"
+
+console.log(`I could
+    really go for some
+    Pad thai right now.🥢`);
+// This outputs:
+//			"I could
+//  really go for some
+//  Pad thai right now.🥢"
