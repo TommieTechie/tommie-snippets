b5ed2daf3e84e2aeca57ad74fd326940d996384d
TommiEng <TommiEng@proton.me>
2022-07-12 12:58:05 -0400

    Added custom component and prop styling in React snippet

---
+++ snippets/react-styling-07-07-2022/react-custom-styling-12-07-2022.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "../planet-styles.css";
+
+const Planet = ({style, planet, className = "", children}) => {
+  
+  const planetClassName = planet ? `planet--${planet}` : "";
+
+  return (
+    <div className={`planet ${className} ${planetClassName}`} style={{fontWeight: "bold", ...style}}>
+      {children}
+    </div>
+  );
+}
+
+
+const SolarSystemApp = () => {
+  return (
+    <div>
+      <Planet planet="mars" style={{backgroundColor: "orange"}}>
+        Mars
+      </Planet>
+      <Planet planet="earth" style={{backgroundColor: "blue", color: "white"}}>
+        Earth
+      </Planet>
+      <Planet>
+        Unknown
+      </Planet>
+    </div>
+  );
+}
+
+export default SolarSystemApp;
