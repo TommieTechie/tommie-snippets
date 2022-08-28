2cf9dc88ff8eb9c4bc853997e01e16329e62803c
TommiEng <TommiEng@proton.me>
2022-08-27 18:48:41 -1000

    Add how to convert string to number snippet

---
+++ snippets/js-convert-string-to-number-16-03-2022.js
@@ -0,0 +1,8 @@
+const stringSeven = "7";
+// typeof stringSeven returns "string"
+
+const numSeven = parseInt(stringSeven);
+// typeof numSeven returns "number"
+
+const numberSeven = Number(stringSeven);
+// typeof numberSeven also returns "number"
