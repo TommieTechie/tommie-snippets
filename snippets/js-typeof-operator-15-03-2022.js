2f9c13500e52fa0ef5f236c75e5d627b7232ef7f
TommiEng <TommiEng@proton.me>
2022-08-28 22:06:08 -1000

    Added typeof operator snippet

---
+++ snippets/js-typeof-operator-15-03-2022.js
@@ -0,0 +1,14 @@
+console.log(typeof "LGBTQ+ in Tech");
+// Returns: "string"
+
+console.log(typeof 1234);
+// Returns: "number"
+
+console.log(typeof false);
+// Returns: "boolean"
+
+if (typeof variable == "undefined") {
+  // Catch the error...
+}
+
+// Other types include null, undefined, function.
