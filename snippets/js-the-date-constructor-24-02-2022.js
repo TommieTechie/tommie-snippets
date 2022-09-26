6b389bca1717380f2a207302c1d344b1d8919b2a
TommiEng <TommiEng@proton.me>
2022-09-25 21:21:02 -0400

    Added the Date constructor snippet

---
+++ snippets/js-the-date-constructor-24-02-2022.js
@@ -0,0 +1,11 @@
+// JavaScript date constructor, returns today's date
+const today = new Date();
+
+console.log(today.getDay());
+// Today's day
+
+console.log(today.getMonth());
+// Today's month, the month ranging from 0 - 11
+
+console.log(today.getFullYear());
+// Today's year, which would be 2022
